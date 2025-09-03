f7235ed12970e708b5748c4e6e841c683060eb5b
laila33 <lailaabdelnaser64@gmail.com>
2025-09-03 20:42:13 +0300

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Cards from "./Card";
-// import FirstSection from "./FirstSection";
-// import Secondsection from "./Secondsection";
+import FirstSection from "./FirstSection";
+import Secondsection from "./secondsection";
 
 export default function App() {
   return (
